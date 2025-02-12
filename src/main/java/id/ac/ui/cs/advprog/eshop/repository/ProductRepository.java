88df6c3a7ba509075422f28778f36b725134d7f3
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-02-13 01:43:40 +0700

    delete product sudah bisa dijalankan

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -34,12 +34,18 @@ public class ProductRepository {
     }
 
     public Product update(Product updatedProduct) {
-        for (int i = 0; i < productData.size(); i++){
-            if (productData.get(i).getProductId().equals(updatedProduct.getProductId())){
-                productData.set(i, updatedProduct);
-                return updatedProduct;
-            }
+        Product productToEdit = findById(updatedProduct.getProductId());
+        if (productToEdit != null) {
+            productToEdit.setProductName(updatedProduct.getProductName());
+            productToEdit.setProductQuantity(updatedProduct.getProductQuantity());
         }
-        return null;
+        return productToEdit;
+    }
+
+    public void delete(String id) {
+        Product deletedProduct = findById(id);
+        if (deletedProduct != null) {
+            productData.remove(deletedProduct);
+        }
     }
 }
