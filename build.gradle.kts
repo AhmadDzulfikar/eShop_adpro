5ebb3440439aa58825d179690239bd785e4a1546
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-02-20 21:55:15 +0700

    membuat ProuctControllerTest untuk codecoverage 100%

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("java")
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -26,8 +27,9 @@ repositories {
 
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
+val webdrivemanagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
+val mockitoVersion = "5.2.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -38,27 +40,29 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-	testImplementation ("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivemanagerVersion")
 	testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+	testImplementation("org.mockito:mockito-inline:$mockitoVersion")
+	testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
 
-
-tasks.register<Test>( "unitTest") {
+tasks.register<Test>("unitTest") {
 	description = "Runs unit tests."
 	group = "verification"
 
 	filter {
-		excludeTestsMatching ("*FunctionalTest")
+		excludeTestsMatching("*FunctionalTest")
 	}
 }
-tasks.register<Test>( "functionalTest") {
+
+tasks.register<Test>("functionalTest") {
 	description = "Runs functional tests."
 	group = "verification"
+
 	filter {
-		includeTestsMatching ("*FunctionalTest")
+		includeTestsMatching("*FunctionalTest")
 	}
 }
 
@@ -67,7 +71,7 @@ tasks.withType<Test>().configureEach {
 }
 
 tasks.test{
-	filter {
+	filter{
 		excludeTestsMatching("*FunctionalTest")
 	}
 	finalizedBy(tasks.jacocoTestReport)
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
