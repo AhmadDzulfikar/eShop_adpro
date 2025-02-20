75040e48163f95e2190067b348f1cef67dd33ccd
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-02-21 02:01:20 +0700

    menyelesaikan (This utility class has a non-private constructor)

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
-
+	@SuppressWarnings("PMD.UseUtilityClass")
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
