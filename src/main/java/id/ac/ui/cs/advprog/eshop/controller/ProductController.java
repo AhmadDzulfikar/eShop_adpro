85b21cbde972a9a44a52c756523204a280f7fb60
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-02-27 20:53:35 +0700

    sudah menyelesaikan bagian before-solid

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -84,7 +84,7 @@ public class ProductController {
             return "CarList";
         }
 
-        @GetMapping("/editCar/{carId")
+        @GetMapping("/editCar/{carId}")
         public String editCarPage(@PathVariable String carId, Model model) {
             Car car = carservice.findById(carId);
             model.addAttribute("car", car);
