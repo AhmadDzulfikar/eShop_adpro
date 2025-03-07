3e37dde258dd26452eda57093cd06f9b07b22751
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-03-07 16:54:59 +0700

    [REFACTOR] Add PaymentStatus enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -5,19 +5,22 @@ import lombok.Getter;
 @Getter
 public enum PaymentStatus {
     SUCCESS("SUCCESS"),
+    PENDING("PENDING"),
     REJECTED("REJECTED");
 
     private final String value;
-    PaymentStatus(String value) {
+
+    private PaymentStatus(String value){
         this.value = value;
     }
 
-    public static boolean contains(String param) {
-        for (PaymentStatus status : PaymentStatus.values()) {
-            if (status.name().equals(param)) {
+    public static boolean contains(String param){
+        for(PaymentStatus paymentStatus: PaymentStatus.values()){
+            if(paymentStatus.name().equals(param)){
                 return true;
             }
         }
         return false;
     }
+
 }
